a78d26d026843f45520855a1731ab50999ef2192
Nikhil Limaye <nikhillimaye10@gmail.com>
2019-08-28 15:35:27 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-
 const connectdb = async () => {
     try {
         await mongoose.connect(db , {
